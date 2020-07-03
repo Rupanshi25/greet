965510d5fab6e46c6981e265bd3623dfc979ca73
Rupanshi25 <rupanshijain2000@gmail.com>
2020-07-04 00:47:57 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let curDate= new Date();
+curDate = curDate.getHours();
+let greeting= '';
+const cssStyle= {};
+
+if(curDate >=1 && curDate < 12)
+{
+  greeting="Good Morning";
+  cssStyle.color='Red';
+}
+else if(curDate >= 12 && curDate < 19)
+{
+  greeting= "Good Afternoon";
+  cssStyle.color='Blue';
+}
+ else
+{
+  greeting= "Good Night";
+  cssStyle.color='black';
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+ <div>
+  <h1>Hello sir, <span style={cssStyle}>{ greeting }</span></h1>
+  </div>
+  </>,
   document.getElementById('root')
+  
 );
 
 // If you want your app to work offline and load faster, you can change
